11e6becd89f8f9717c877e1623f2f9590d7981d1
MrJackSpade <loffredo.ap@gmail.com>
2020-05-04 08:01:24 -0700

    Auto Commit

--- IProperty.cs
+++ IProperty.cs
@@ -1,4 +1,8 @@
-﻿using Penguin.Reflection.Serialization.Abstractions.Interfaces;
+﻿using Penguin.Reflection.Extensions;
+using Penguin.Reflection.Serialization.Abstractions.Interfaces;
+using Penguin.Reflection.Serialization.Objects;
+using System;
+using System.Collections.Generic;
 
 namespace Penguin.Reflection.Serialization.Extensions
 {
@@ -18,6 +22,16 @@ namespace Penguin.Reflection.Serialization.Extensions
         /// <returns>The source property casted to the specified type</returns>
         public static T GetValue<T>(this IMetaProperty p, IMetaObject target)
         {
+            if (p is null)
+            {
+                throw new System.ArgumentNullException(nameof(p));
+            }
+
+            if (target is null)
+            {
+                throw new System.ArgumentNullException(nameof(target));
+            }
+
             return target.GetValue<T>(p.Name);
         }
 
@@ -29,7 +43,102 @@ namespace Penguin.Reflection.Serialization.Extensions
         /// <returns>The value of the property from the speficied source as a string</returns>
         public static string GetValue(this IMetaProperty p, IMetaObject target)
         {
+            if (p is null)
+            {
+                throw new System.ArgumentNullException(nameof(p));
+            }
+
+            if (target is null)
+            {
+                throw new System.ArgumentNullException(nameof(target));
+            }
+
             return target[p.Name].Value;
+        }
+
+        public static bool TestFlags(long val, long flags)
+        {
+            if (val == 0)
+            {
+                return flags == 0;
+            }
+
+            if (flags == 0)
+            {
+                return val == 0;
+            }
+
+            for (int i = 0; i < 64; i++)
+            {
+                if (((flags >> i) & 0x1) != 0)
+                {
+                    if (((val >> i) & 0x1) == 0)
+                    {
+                        return false;
+                    }
+                }
+            }
+
+            return true;
+        }
+
+        /// <summary>
+        /// Gets flags set on an enum property
+        /// </summary>
+        /// <param name="p">The propertyInfo</param>
+        /// <param name="target">The instance of the object</param>
+        /// <param name="otherFlags">returns a long representing the value of all flags on the object value that aren't declared explicitely</param>
+        /// <returns>A list of the set enum values</returns>
+        public static IList<EnumValue> GetFlags(this IMetaProperty p, IMetaObject target, out long otherFlags)
+        {
+            otherFlags = p.GetValue(target).Convert<long>();
+
+            List<EnumValue> toReturn = new List<EnumValue>();
+
+            foreach (EnumValue thisValue in p.GetFlags(target))
+            {
+                toReturn.Add(thisValue);
+
+                otherFlags &= ~thisValue.Value.Convert<long>();
+            }
+
+            return toReturn;
+        }
+
+        /// <summary>
+        /// Gets flags set on an enum property
+        /// </summary>
+        /// <param name="p">The propertyInfo</param>
+        /// <param name="target">The instance of the object</param>
+        /// <returns>A list of the set enum values</returns>
+        public static IEnumerable<EnumValue> GetFlags(this IMetaProperty p, IMetaObject target)
+        {
+            if (p is null)
+            {
+                throw new System.ArgumentNullException(nameof(p));
+            }
+
+            if (target is null)
+            {
+                throw new System.ArgumentNullException(nameof(target));
+            }
+
+            if (!p.Type.HasAttribute<FlagsAttribute>())
+            {
+                throw new ArgumentException($"Property type {p.Type.FullName} does not have flags attribute");
+            }
+
+            long l = p.GetValue(target).Convert<long>();
+
+            foreach (EnumValue thisValue in p.Type.Values)
+            {
+                long thisVal = thisValue.Value.Convert<long>();
+
+                if (TestFlags(l, thisVal))
+                {
+                    yield return thisValue;
+                }
+            }
         }
 
         #endregion Methods
